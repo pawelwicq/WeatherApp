2f8b9e9b097447136ed283a4c3f5d7ed3e78ef4f
pawel <pawelwicq@gmail.com>
2019-06-26 20:35:43 +0200

    Added some data to details screen layout

--- app/src/main/java/com/wickowski/weatherapp/presentation/city_weather_details/CityWeatherDetailsFragment.kt
+++ app/src/main/java/com/wickowski/weatherapp/presentation/city_weather_details/CityWeatherDetailsFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.wickowski.weatherapp.R
+import com.wickowski.weatherapp.presentation.CityCurrentWeather
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 import java.lang.IllegalStateException
 import com.wickowski.weatherapp.presentation.city_weather_details.CityWeatherDetailsViewModel.*
 import com.wickowski.weatherapp.utils.showToast
+import kotlinx.android.synthetic.main.fragment_city_weather_details.*
+import kotlinx.android.synthetic.main.layout_current_weather_details_card.*
 
 private const val CITY_ID_BUNDLE_EXTRA = "CITY_ID"
 
@@ -36,6 +40,7 @@ class CityWeatherDetailsFragment : Fragment() {
         cityId = arguments?.getString(CITY_ID_BUNDLE_EXTRA)
             ?: throw IllegalStateException("City ID must be provided to this fragment")
         viewModel.loadCityWeatherDetails()
+        closeBtn.setOnClickListener { findNavController().navigateUp() }
     }
 
     override fun onResume() {
@@ -46,8 +51,16 @@ class CityWeatherDetailsFragment : Fragment() {
 
     private fun handleWeatherState(state: CityWeatherDetailsState) = when (state) {
         CityWeatherDetailsState.Loading -> showToast("Loading")
-        is CityWeatherDetailsState.Success -> showToast("Success")
+        is CityWeatherDetailsState.Success -> updateCurrentWeatherData(state.currentWeather)
         is CityWeatherDetailsState.Error -> showToast("Err")
+    }
+
+    private fun updateCurrentWeatherData(currentWeather: CityCurrentWeather) = with(currentWeather) {
+        city.text = cityName
+        temperatureValue.text = getString(R.string.celsius_temperature, temperature)
+        currentWeatherIcon.setAnimation(weatherIcon)
+        pressureValue.text = getString(R.string.h_pascal_pressure, pressure)
+        currentWeatherDescription.text = getString(conditionStringRes).capitalize()
     }
 
 }
